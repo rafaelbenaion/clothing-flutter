67bd2366d3468a1365bc92f7196814209147b31b
Rafael Baptista <rafaelbaptista@Rafaels-MacBook-Air.local>
2024-11-07 23:39:10 +0100

    AI model for category detection

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -14,7 +14,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart'; // For text input formatting
-import 'test.dart';  // Import the OnnxService class
+import 'onnx_service.dart';  // Import the OnnxService class
 import 'dart:ui' as ui; // Ensure to import for image processing
 import 'package:image/image.dart' as img; // Add this import
 
